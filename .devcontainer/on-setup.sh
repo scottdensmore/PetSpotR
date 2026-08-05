d526964ebe741958a8440f89717f15f3fd6844b3
Scott Densmore <scottdensmore@mac.com>
2026-08-05 10:06:48 -0700

    chore(repo): remove legacy Python, Blazor, Bicep, and Workshop dead code (#40) (#41)

--- .devcontainer/on-setup.sh
+++
@@ -1,7 +0,0 @@
-# Install dapr cli
-wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
-
-# Initialize dapr
-dapr uninstall --all # clean if needed
-
-scripts/setup
